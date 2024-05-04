e054c89c0359b08d6b7133bf418a45b58f6eeb84
Spencer Snyder <sasnyde2@gmail.com>
2024-05-03 20:21:50 -0400

    feat: adds basic pricing cards

--- src/controllers/pricing.tsx
+++ src/controllers/pricing.tsx
@@ -1,17 +1,62 @@
+/* eslint-disable react/style-prop-object */
 import type {Middleware} from 'koa';
 import Layout from '../../src/components/layout.js';
 
 export const get: Middleware = async (ctx) => {
   await ctx.renderRaw(
     <Layout>
-      <div class="d-flex justify-content-center mb-5">
-        <h1>Pricing</h1>
-      </div>
-      <div class="border border-warning rounded p-3">
-        <p>
-          Our pricing is simple and straightforward. You pay a flat rate of $10
-          per month for unlimited access to all of our features.
-        </p>
+      <div class="d-flex flex-column justify-content-center">
+        <h1 class="mb-5 align-self-center">Pricing</h1>
+        <div class="border-start border-3 border-warning rounded p-3 align-self-center mb-5">
+          <p>
+            Our pricing is simple and straightforward. You pay a flat rate of
+            $10 per month for unlimited access to all of our features.
+          </p>
+        </div>
+        <div
+          class="d-flex justify-content-around flex-wrap"
+          style="height: 30rem;"
+        >
+          <div
+            class="card m-2 text-center border-warning"
+            style="width: 18rem;"
+          >
+            <img src="/favicon-32x32.png" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title text-center">Free</h5>
+              <p class="card-text h1">$10</p>
+              <a href="#" class="btn btn-warning">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+          <div
+            class="card m-2 text-center border-warning"
+            style="width: 18rem;"
+          >
+            <img src="/favicon-32x32.png" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title text-center">Pro</h5>
+              <p class="card-text h1">$10</p>
+              <a href="#" class="btn btn-warning">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+          <div
+            class="card m-2 text-center border-warning"
+            style="width: 18rem;"
+          >
+            <img src="/favicon-32x32.png" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title text-center">Executive</h5>
+              <p class="card-text h1">$10</p>
+              <a href="#" class="btn btn-warning">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </Layout>,
   );
