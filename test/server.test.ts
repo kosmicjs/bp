53cd1ce710d4907d2206f457449e7504dacf2131
Spencer Snyder <sasnyde2@gmail.com>
2024-05-18 19:07:39 -0400

    feat: fix test file cleanup

--- test/server.test.ts
+++ test/server.test.ts
@@ -1,16 +1,24 @@
-import {describe, test, expect} from 'vitest';
+import {describe, test, expect, beforeAll, afterAll} from 'vitest';
 import got from 'got';
 import {app} from '../src/server.js';
+import {type Kosmic} from '../packages/core/index.js';
 
 describe('server integration', async () => {
-  await app.start({
-    host: 'localhost',
-    port: 4567,
+  let server: Kosmic;
+  beforeAll(async () => {
+    server = await app.start({
+      host: 'localhost',
+      port: 4567,
+    });
   });
 
   test('GET / 200 ok', async () => {
     const response = await got('http://localhost:4567');
 
     expect(response.statusCode).toEqual(200);
+  });
+
+  afterAll(async () => {
+    await server.close();
   });
 });
