f35ebb34b024193d0d34e2b43521f521a97fcc73
Spencer Snyder <sasnyde2@gmail.com>
2025-02-16 13:15:40 -0500

    chore: fix up basic tests

--- src/middleware/router/index.test.ts
+++
@@ -1,6 +0,0 @@
-import {test, expect} from 'vitest';
-import {createFsRouter} from './index.js';
-
-test('createFsRouter', async () => {
-  expect(typeof createFsRouter).is('function');
-});
--- test/server.test.ts
+++ test/server.test.ts
@@ -1,23 +1,24 @@
 import {type Server} from 'node:http';
-import {describe, test, expect, beforeAll, afterAll} from 'vitest';
+import {describe, test, before, after} from 'node:test';
+import {strictEqual} from 'node:assert';
 import got from 'got';
 import {createServer} from '../src/server.js';
 
-describe('server integration', async () => {
+await describe('server integration', async () => {
   let server: Server;
 
-  beforeAll(async () => {
+  before(async () => {
     server = await createServer();
     server.listen(4567);
   });
 
-  test('GET / 200 ok', async () => {
+  await test('GET / 200 ok', async () => {
     const response = await got('http://localhost:4567');
 
-    expect(response.statusCode).toEqual(200);
+    strictEqual(response.statusCode, 200);
   });
 
-  afterAll(() => {
+  after(() => {
     server.closeAllConnections();
     server.close();
   });
